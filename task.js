3067383e2a699e414524018caf6b78856cd443ef
Jonnalagadda Lakshmi Prasanna <142923910+jlakshmiprasanna@users.noreply.github.com>
2025-02-04 13:52:31 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    alert("ordered finish")
+},1000)
+
+setTimeout(()=>{
+    alert("shipped")
+},10000)
+
+setTimeout(() => {
+  alert("delivered");
+}, 12000);
+
+setTimeout(() => {
+  alert("order received");
+}, 15000);
